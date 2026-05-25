693b4e7b38651219e4b1fbdd9f4634984db62df5
realarmaansidhu <justarmaansidhu@gmail.com>
2026-05-24 21:15:13 -0400

    Updated OpenAI references with Groq

--- Academy/module-4/studio/parallelization.py
+++ Academy/module-4/studio/parallelization.py
@@ -8,11 +8,11 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_community.document_loaders import WikipediaLoader
 from langchain_tavily import TavilySearch  # updated 1.0
 
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 
 from langgraph.graph import StateGraph, START, END
 
-llm = ChatOpenAI(model="gpt-4o", temperature=0) 
+llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0)
 
 class State(TypedDict):
     question: str
